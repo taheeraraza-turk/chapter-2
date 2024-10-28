d207d1439fde130025afb00ae26ebeb69dece7e0
Taheera Raza Turk <taheeraraza0@gmail.com>
2024-10-28 10:35:10 -0700

    Add files via upload

---
+++ chapter 2/JS/app.js
@@ -0,0 +1,44 @@
+// 1. Declare a variable called username.
+        let username;
+
+// 2. Declare a variable called myName & assign to it a string that represents your Full Name.
+        let myName = "Taheera Raza Turk";
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+        let message;
+// b) Assign “Hello World” to variable message
+         message = "Hello World!";
+// c) Display the message in alert box.
+         alert(message);
+
+// 4. Write a script to save student’s bio data in JS variables and show the data in alert boxes
+       let studentname = "Taheera";
+       let age = 15 + "years old";
+       let certification="Certified Web & app developement";
+
+       alert(studentname);
+       alert(age);
+       alert(certification);
+
+// 5. Write a script to display the following alert using one JS variable:
+       let pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P ";
+       alert(pizza);
+
+// 6. Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com). Show the blow mentioned message in an alert box.(Hint: use string concatenation).
+       let email = "taheeraraza0@gmail.com";
+       alert("My email address is" + email);
+
+// 7. Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following message in an alert box:
+        let book = "A smarter way to learn JavaScript";
+        alert("I am trying to learn from the book" + book);
+
+// 9. Store following string in a variable and show in alert and browser through JS
+         let design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+         alert(design);
+
+
+
+
+
+       
